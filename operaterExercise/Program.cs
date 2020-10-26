05c328fe0cce5db82b3c9c8d58b8a48a27394115
dnyor818 <36803265+dnyor818@users.noreply.github.com>
2020-10-26 12:17:48 -0600

    Add files via upload

---
+++ operaterExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace operaterExercise
+{
+    class Program
+    {
+
+        public static double AreaofCircle(double r)
+        {
+            double pi = 3.14;
+
+            return pi * (r  * r);
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("what is the radius?");
+
+            double r = Double.Parse(Console.ReadLine());
+
+           var area = AreaofCircle(r);
+            Console.WriteLine($"The area of your circle is {area}");
+
+
+           
+
+
+        }
+
+    }
+}
